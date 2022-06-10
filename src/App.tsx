ebc4c7a7819aa7d7517ca03253df8ecf2f00d5d1
Jiří Vrba <jiri@vrba.dev>
2022-06-10 21:30:33 +0200

    Improve tester selection component

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ import { QuestionSet } from './types';
 const App: React.FC = () => {
   const [questionSet, setQuestionSet] = useState<QuestionSet>(sets[0]);
 
-  const select = (subject: string) => {
-    window.localStorage.setItem("current-set", subject);
-    setQuestionSet(sets.filter(set => set.subject === subject)[0]);
+  const select = (subject: QuestionSet) => {
+    window.localStorage.setItem("current-set", subject.subject);
+    setQuestionSet(subject);
   }
 
   useEffect(() => {
     const subject = window.localStorage.getItem("current-set") ?? sets[0].subject;
     const [selected] = sets.filter(set => set.subject === subject);
 
-    select(selected.subject);
+    select(selected);
   }, []);
 
   return (
@@ -23,7 +23,7 @@ const App: React.FC = () => {
       <header className="flex flex-col p-10 bg-gray-100">
         <h1 className="text-3xl font-bold text-gray-600">{questionSet.title}</h1>
         <p className="text-sm text-gray-400 uppercase font-bold tracking-widest mt-3">Tester obsahuje {questionSet.questions.length} otázek</p>
-        <TesterSelection selected={questionSet.subject} subjects={sets.map(set => set.subject)} onSelect={select}/>
+        <TesterSelection selected={questionSet} sets={sets} onSelect={select}/>
       </header>
     </>
   );
--- src/components/TesterSelection.tsx
+++ src/components/TesterSelection.tsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
+import { QuestionSet } from "../types";
 
 export interface TesterSelectionProps {
-    subjects: Array<string>,
-    selected?: string | undefined,
-    onSelect: (selected: string) => void
+    sets: Array<QuestionSet>,
+    selected?: QuestionSet,
+    onSelect: (selected: QuestionSet) => void
 }
 
-const TesterSelection: React.FC<TesterSelectionProps> = ({ subjects, selected, onSelect }: TesterSelectionProps) => {
+const TesterSelection: React.FC<TesterSelectionProps> = ({ sets, selected, onSelect }: TesterSelectionProps) => {
     const [expanded, setExpanded] = useState<boolean>(false);
 
-    const select = (subject: string): void => {
+    const select = (subject: QuestionSet): void => {
         onSelect(subject);
         setExpanded(false);
     }
 
     return (
-        <div className="absolute p-5 rounded-xl top-0 right-0 transition" onMouseLeave={() => setExpanded(false)}>
+        <div className="absolute flex flex-col p-5 rounded-xl top-0 right-0 transition items-end" onMouseLeave={() => setExpanded(false)}>
             <button onMouseEnter={() => setExpanded(true)} onClick={() => setExpanded(expanded => !expanded)} className={`cursor-pointer px-5 py-3 bg-white border-2 rounded-lg uppercase text-gray-500 tracking-widest font-bold transition flex flex-row items-center ${expanded ? 'border-gray-400 shadow-lg' : 'border-gray-200 hover:border-gray-400'}`}>
-                {selected}
+                {selected?.subject}
                 <FaChevronDown className={`ml-3 transition transform ${expanded ? 'rotate-180' : 'rotate-0'}`} />
             </button>
             <div className={`${expanded ? 'visible' : 'invisible'}`}>
                 {
-                    subjects
+                    sets
                         .filter(subject => subject !== selected)
                         .map(subject =>
-                            <div key={subject} onClick={() => select(subject)} className={`block text-center cursor-pointer px-5 py-3 mt-3 bg-white text-gray-500 border-2 border-gray-200 rounded-lg uppercase tracking-widest font-bold transition transform ${expanded ? 'shadow-lg opacity-1 translate-y-0 hover:bg-blue-100 hover:text-blue-700 hover:border-blue-500' : 'opacity-0 -translate-y-10'}`}>
-                                {subject}
+                            <div key={subject.title} onClick={() => select(subject)} className={`flex flex-col cursor-pointer px-5 py-3 mt-3 bg-white border-2 border-gray-200 rounded-lg transition transform group ${expanded ? 'shadow-lg opacity-1 translate-y-0 hover:bg-white hover:border-black' : 'opacity-0 -translate-y-10'}`}>
+                                <h1 className="transition text-gray-400 group-hover:text-blue-500 uppercase font-black tracking-widest">{subject.subject}</h1>
+                                <span className="transition text-gray-400 group-hover:text-black tracking-wide font-bold">{subject.title}</span> 
                             </div>
                         )
                 }
