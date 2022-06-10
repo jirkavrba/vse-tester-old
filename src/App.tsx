84f9b1aeca7e08a25ae4c1460ceb4bf19e16d2d3
Jiří Vrba <jiri@vrba.dev>
2022-06-10 21:02:34 +0200

    Implement subject switching logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TesterSelection from './components/TesterSelection';
 import sets from "./sets"
 import { QuestionSet } from './types';
@@ -6,9 +6,25 @@ import { QuestionSet } from './types';
 const App: React.FC = () => {
   const [questionSet, setQuestionSet] = useState<QuestionSet>(sets[0]);
 
+  const select = (subject: string) => {
+    window.localStorage.setItem("current-set", subject);
+    setQuestionSet(sets.filter(set => set.subject === subject)[0]);
+  }
+
+  useEffect(() => {
+    const subject = window.localStorage.getItem("current-set") ?? sets[0].subject;
+    const [selected] = sets.filter(set => set.subject === subject);
+
+    select(selected.subject);
+  }, []);
+
   return (
     <>
-      <TesterSelection subjects={sets.map(set => set.subject)} />
+      <header className="flex flex-col p-10 bg-gray-100">
+        <h1 className="text-3xl font-bold text-gray-600">{questionSet.title}</h1>
+        <p className="text-sm text-gray-400 uppercase font-bold tracking-widest mt-3">Tester obsahuje {questionSet.questions.length} otázek</p>
+        <TesterSelection subjects={sets.map(set => set.subject)} onSelect={select}/>
+      </header>
     </>
   );
 }
--- src/components/TesterSelection.tsx
+++ src/components/TesterSelection.tsx
@@ -1,29 +1,33 @@
 import { useState } from "react";
+import { FaChevronDown } from "react-icons/fa";
 
 export interface TesterSelectionProps {
-    subjects: Array<string>
+    subjects: Array<string>,
+    onSelect: (selected: string) => void
 }
 
-const TesterSelection: React.FC<TesterSelectionProps> = ({ subjects }: TesterSelectionProps) => {
+const TesterSelection: React.FC<TesterSelectionProps> = ({ subjects, onSelect }: TesterSelectionProps) => {
     const [selected, setSelected] = useState<string>(subjects[0]);
     const [expanded, setExpanded] = useState<boolean>(false);
 
     const select = (subject: string): void => {
+        onSelect(subject);
         setSelected(subject);
         setExpanded(false);
     }
 
     return (
         <div className="absolute p-5 rounded-xl top-0 right-0 transition" onMouseLeave={() => setExpanded(false)}>
-            <button onMouseEnter={() => setExpanded(true)} onClick={() => setExpanded(expanded => !expanded)} className={`cursor-pointer px-5 py-3 bg-white border-2 rounded-lg uppercase text-gray-500 tracking-widest font-bold transition ${expanded ? 'border-gray-400 shadow-lg' : 'border-gray-200 hover:border-gray-400'}`}>
+            <button onMouseEnter={() => setExpanded(true)} onClick={() => setExpanded(expanded => !expanded)} className={`cursor-pointer px-5 py-3 bg-white border-2 rounded-lg uppercase text-gray-500 tracking-widest font-bold transition flex flex-row items-center ${expanded ? 'border-gray-400 shadow-lg' : 'border-gray-200 hover:border-gray-400'}`}>
                 {selected}
+                <FaChevronDown className={`ml-3 transition transform ${expanded ? 'rotate-180' : 'rotate-0'}`}/>
             </button>
             <div className={`${expanded ? 'visible' : 'invisible'}`}>
                 {
                     subjects
                         .filter(subject => subject !== selected)
                         .map(subject =>
-                            <div key={subject} onClick={() => select(subject)} className={`block cursor-pointer px-5 py-3 mt-3 bg-gray-50 text-gray-500 border-2 border-gray-200 rounded-lg uppercase tracking-widest font-bold transition transform ${expanded ? 'shadow-lg opacity-1 translate-y-0 hover:bg-gray-200 hover:text-gray-700 hover:border-gray-400' : 'opacity-0 -translate-y-10'}`}>
+                            <div key={subject} onClick={() => select(subject)} className={`block text-center cursor-pointer px-5 py-3 mt-3 bg-white text-gray-500 border-2 border-gray-200 rounded-lg uppercase tracking-widest font-bold transition transform ${expanded ? 'shadow-lg opacity-1 translate-y-0 hover:bg-blue-100 hover:text-blue-700 hover:border-blue-500' : 'opacity-0 -translate-y-10'}`}>
                                 {subject}
                             </div>
                         )
