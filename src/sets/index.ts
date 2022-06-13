d179f01dbf202ed721f4cc736828ddc2365f2c8f
Jiří Vrba <jiri@vrba.dev>
2022-06-13 19:33:57 +0200

    Add question set for 4it115
    Co-authored-by: Kreamis <92675028+Kreamis@users.noreply.github.com>

--- src/sets/index.ts
+++ src/sets/index.ts
@@ -3,6 +3,7 @@ import { QuestionSet } from "../types";
 import questions3sg201 from "./3sg201-strategicka-analyza.json";
 import questions4sa310 from "./4sa310-it-governance.json";
 import questions4st204 from "./4st204-statistika-pro-informatiky.json";
+import questions4it115 from "./4it115-softwarove-inzenyrstvi.json";
 
 const sets: Array<QuestionSet> = [
     {
@@ -19,6 +20,11 @@ const sets: Array<QuestionSet> = [
         title: "Statistika pro informatiky",
         subject: "4ST204",
         questions: questions4st204
+    },
+    {
+        title: "Softwarové inženýrství",
+        subject: "4IT115",
+        questions: questions4it115
     }
 ];
 
